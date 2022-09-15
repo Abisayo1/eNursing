a3fb39860d9088adb91f96112ca6556cf25a709c
Abisayo <foluagoiabisayo@gmail.com>
2022-09-15 21:10:19 +0100

    THis is Computewrize 1

--- app/src/main/java/com/abisayo/computerize1/CreateAccount.kt
+++ app/src/main/java/com/abisayo/computerize1/CreateAccount.kt
@@ -1,11 +1,21 @@
 package com.abisayo.computerize1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abisayo.computerize1.databinding.ActivityCreateAccountBinding
+import com.abisayo.computerize1.databinding.ActivityMainBinding
 
 class CreateAccount : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView8.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

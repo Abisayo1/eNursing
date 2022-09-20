cabdd18c3193e218b8dfe20d27810406c5455fa5
Abisayo <foluagoiabisayo@gmail.com>
2022-09-20 17:55:48 +0100

    Built the First Topic

--- app/src/main/java/com/abisayo/computerize1/Learn.kt
+++ app/src/main/java/com/abisayo/computerize1/Learn.kt
@@ -1,11 +1,80 @@
 package com.abisayo.computerize1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.abisayo.computerize1.databinding.ActivityLearnBinding
+import com.abisayo.computerize1.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import android.system.Os.close
+import android.view.Menu
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class Learn : AppCompatActivity() {
+    private lateinit var binding: ActivityLearnBinding
+    lateinit private var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_learn)
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        binding = ActivityLearnBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.nav_home -> Toast.makeText(applicationContext, "Home!", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+            true
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        if (item.itemId == R.id.action_item_one) {
+            val intent = Intent(this, Topics::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+
+
     }
 }
+
+
+
+
